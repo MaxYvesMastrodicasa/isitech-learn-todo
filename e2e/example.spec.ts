0ae21c3cdd7d7bc90b62d5c710442d5b2b7fd943
mymastrodicasa <maxyves.mastrodicasa@gmail.com>
2024-08-28 11:52:02 +0200

    eigth

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -5,10 +5,14 @@ test('should add a new todo item', async ({ page }) => {
   await page.goto('http://localhost:5173');
 
   const input = page.locator('input[placeholder="Nourrir le chat"]');
-  const addButton = page.locator('text=Add a todo');
+  const btn = page.locator('text=Add a todo');
   const todoList = page.locator('[data-testid="list"]');
 
+
+
   await input.fill('Nourrir le chat');
-  await addButton.click();
+  await btn.click();
+
+  await expect(todoList).toHaveCount(1);
   await expect(todoList).toContainText('Nourrir le chat');
 });
